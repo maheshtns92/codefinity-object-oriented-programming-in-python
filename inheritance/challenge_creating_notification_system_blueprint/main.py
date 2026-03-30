87fc8c9ab12b452a474291e043b8a1531b2ea99a
maheshtns92 <>
2026-03-30 01:49:26 +0000

    Challenge: Creating Notification System Blueprint

---
+++ inheritance/challenge_creating_notification_system_blueprint/main.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+class Notifier(ABC):
+    @abstractmethod
+    def send(self,message):
+        pass
+class EmailNotifier(Notifier):
+    def send(self,message):
+        return f"Email sent:{message}"
+class SMSNotifier(Notifier):
+    def send(self,message):
+        return f"SMS sent:{message}"
+email_notifier = EmailNotifier()
+sms_notifier = SMSNotifier()
+def notify_user(notifier,message):
+    return notifier.send(message)
+    
+
+# Define the abstract base class Notifier
+
+# Define the EmailNotifier subclass
+
+# Define the SMSNotifier subclass
+
+# Define the notify_user function
+
+print(notify_user(email_notifier, "Welcome!"))
+print(notify_user(sms_notifier, "Your code is 1234."))
